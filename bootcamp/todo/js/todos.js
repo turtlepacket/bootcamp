d2bde427eefd80200d9eda8ffde46c3f6ffd3b3e
Mary <malystrasza@gmail.com>
2017-02-01 01:49:39 +0000

    to-do list complete

--- bootcamp/todo/js/todos.js
+++ bootcamp/todo/js/todos.js
@@ -19,8 +19,13 @@ $(document).ready(function(){
             var todoText = $(this).val();
             $(this).val("");
             //create a new li and add to ul
-            $("ul").append("<li><span>X</span> " + todoText + "</li>");
+            $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
         }
+    });
+    
+    
+    $(".fa-plus").click(function(){
+        $("input[type='text']").fadeToggle();
     })
     
 });
